25b0cdf2b72038ea66e5fffd8da07b480f8e6fc0
Raunak Agarwal <ragarwal@uni-potsdam.de>
2021-05-06 07:03:32 +0200

    fix bash script to parse args

--- run_scripts/run_baseline.sh
+++ run_scripts/run_baseline.sh
@@ -1,2 +1,10 @@
-source ../venv/bin/activate
+while getopts u:a:f: flag
+do
+    case "${flag}" in
+        i) InputDataset=${OPTARG};;
+        o) OutputDirectory=${OPTARG};;
+    esac
+done
+
+source /mnt/data/touche-macbeth/venv/bin/activate
 python ../utils/query_base.py -i $InputDataset -o $OutputDirectory
